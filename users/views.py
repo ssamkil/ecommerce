a62b0a6708e4cb9bd047b21d88b7f234e99a2025
7sungcola <donghwa2428@gmail.com>
2026-03-12 15:41:41 +0900

    social login removed due to an issue

--- users/views.py
+++ users/views.py
@@ -13,65 +13,6 @@ from my_settings                     import SECRET_KEY, ALGORITHM
 
 load_dotenv()
 
-BASE_URL = 'http://127.0.0.1:8000/'
-
-KAKAO_CALLBACK_URI = BASE_URL + 'users/kakao/callback/'
-
-def kakao_login(request):
-    client_id = os.environ.get('KAKAO_REST_API_KEY')
-    return redirect(f'https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={KAKAO_CALLBACK_URI}&response_type=code')
-
-def kakao_callback(request):
-    client_id = os.environ.get('KAKAO_REST_API_KEY')
-    client_secret = os.environ.get('KAKAO_SECRET_KEY')
-    code = request.GET.get('code', None)
-
-    if code is None:
-        return JsonResponse({'ERROR': 'failed to get code'}, status=400)
-
-    token_request       = requests.get(f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={KAKAO_CALLBACK_URI}&code={code}&client_secret={client_secret}')
-    token_response_json = token_request.json()
-
-    access_token = token_response_json.get('access_token')
-
-    profile_request = requests.post(
-        'https://kapi.kakao.com/v2/user/me',
-        headers={'Authorization': f'Bearer {access_token}'},
-    )
-
-    profile_json = profile_request.json()
-
-    kakao_account = profile_json.get('kakao_account')
-
-    name = kakao_account.get('profile').get('nickname')
-
-    latest_data = User.objects.latest('id')
-    latest_email_num = latest_data.email.split("@")[0][-1]
-
-    if User.objects.filter(name=name):
-        return JsonResponse({'ERROR' : 'Duplicate Name'}, status=400)
-
-    # temporary for backend usage only until frontend is figured
-    password = '12345678'
-
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-
-    # only for kakao since retrieving email is currently unavailable
-    if User.objects.filter(email='test1@naver.com').exists():
-        User.objects.create(
-            name     = name,
-            email    = 'test' + str(latest_email_num + 1) + '@naver.com',
-            password = hashed_password,
-        )
-    else:
-        User.objects.create(
-            name     = name,
-            email    = 'test1@naver.com',
-            password = hashed_password,
-        )
-
-    return JsonResponse({'MESSAGE': name}, json_dumps_params={'ensure_ascii':False}, status=200)
-
 def index(request):
     return render(request, 'user/index.html', {})
 
